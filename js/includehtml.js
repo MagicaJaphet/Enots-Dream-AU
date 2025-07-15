a0115d721bbbbc45b6d516683c54dda8b79c7359
MagicaJaphet <Linda.Shutter@gmail.com>
2025-07-15 12:12:41 -0400

    AAAAAAAAAAAAAAAAAAAA

--- js/includehtml.js
+++ js/includehtml.js
@@ -1,3 +1,25 @@
+function getBaseUrl(url) {
+    var result = url.hostname;
+    if (result.includes("github")) {
+        result += "Enots-Dream-AU";
+    }
+    return result + url.href.splice(url.hostname.length + 3);
+}
+
+function fixURLs() {
+  var z, i, elmnt, file;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("href");
+    if (file && file[0] == ".") {
+      z[i].href = getBaseUrl(file);
+    }
+  }
+}
+
 /* Code from W3Schools*/
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
@@ -22,6 +44,7 @@ function includeHTML() {
       xhttp.open("GET", file, false);
       xhttp.send();
       /* Exit the function: */
+      fixURLs();
       return;
     }
   }
